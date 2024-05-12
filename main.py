aecc2d4eb38174f2b4c0f47f97b5c9428c7a9281
Thomas George <private@thomas-george.com>
2024-05-12 22:31:28 +0200

    basic setup

--- main.py
+++ main.py
@@ -3,3 +3,28 @@ import sys
 
 # init
 pygame.init()
+
+# settings
+window_width = 1280
+window_height = 720
+clock = pygame.time.Clock()
+display_surface = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Asteroid")
+
+# game loop
+while True:
+    # delta time
+    dt = clock.tick(120) / 1000
+
+    # event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # update
+
+    # draw surface
+
+    # show the 'display surface'
+    pygame.display.update()
